5bcf9fc0e1e990c5aded6db22bc46f0b16e786d0
ajoykar <ajoykar@yahoo.com>
2024-12-21 22:14:08 +0600

    python code file added

---
+++ python.py
@@ -0,0 +1,9 @@
+test_dic = {'country': 'Bangladesh', 'district': 'Chattogram', 3 : 'lucky number' }
+test_strng = "Hello How are you Ajoy?"
+test_list1 = [1,2,'q','b']
+test_list2 = ['*',2,'q','b']
+
+
+print (test_strng[3:8])
+print (test_dic)
+print (test_list1 + test_list2)
