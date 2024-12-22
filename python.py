d405d027c461aa03ace8b2405f98bca23d2c1f81
Ajoy Kar <ajoykar@yahoo.com>
2024-12-22 17:31:48 +0600

    udate command file for new dockerfile jening agent with python & rename jenkins-pythonfile to jenkins-python-Dockerfile

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 test_dic = {'country': 'Bangladesh', 'district': 'Chattogram', 3 : 'lucky number' }
 test_strng = "Hello How are you Ajoy?"
 test_list1 = [1,2,'q','b']
-test_list2 = ['*',2,'q','b']
+test_list2 = ['*',2,'q','b','a']
 
 
 print (test_strng[3:8])
